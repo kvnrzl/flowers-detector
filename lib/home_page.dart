5fa81dcbc60b7aef839502b21d1040e2226a96fb
kvnrzl <mkevinr080400@gmail.com>
2021-05-22 16:50:05 +0700

    initial commit

---
+++ lib/home_page.dart
@@ -0,0 +1,23 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool _isLoading;
+  File _image;
+  List _output;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: ,
+      ),
+    );
+  }
+}
